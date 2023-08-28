a89cc20cfdd9fb7cd6488b8b5bca7d15e1816e04
hmtmcse <hmtm.cse@gmail.com>
2023-08-28 22:54:56 +0600

    Working on Swagger UI Generation

--- application/pwebe-basic/pwebe_basic/controller/rest_api_controller.py
+++ application/pwebe-basic/pwebe_basic/controller/rest_api_controller.py
@@ -2,45 +2,45 @@ from pweb import Blueprint, pweb_endpoint, pweb_paginate_endpoint
 from pweb_form_rest import pweb_upload_endpoint
 from pwebe_basic.dto.person_dto import PersonCreateDTO, PersonUpdateDTO, PersonDetailsDTO, PersonUploadDTO
 
-url_prefix = "/"
-rest_controller = Blueprint(
-    "rest_controller",
+url_prefix = "/api/v1/rest"
+rest_api_controller = Blueprint(
+    "rest_api_controller",
     __name__,
     url_prefix=url_prefix
 )
 
 
-@rest_controller.route("/create", methods=['POST'])
+@rest_api_controller.route("/create", methods=['POST'])
 @pweb_endpoint(request_obj=PersonCreateDTO, pweb_message_response=True)
 def create():
     return ""
 
 
-@rest_controller.route("/details/<int:id>", methods=['GET'])
+@rest_api_controller.route("/details/<int:id>", methods=['GET'])
 @pweb_endpoint(response_obj=PersonDetailsDTO)
 def details(id: int):
     return ""
 
 
-@rest_controller.route("/update", methods=['POST'])
+@rest_api_controller.route("/update", methods=['POST'])
 @pweb_endpoint(request_obj=PersonUpdateDTO, pweb_message_response=True)
 def update():
     return ""
 
 
-@rest_controller.route("/delete/<int:id>", methods=['DELETE'])
+@rest_api_controller.route("/delete/<int:id>", methods=['DELETE'])
 @pweb_endpoint()
 def delete(id: int):
     return ""
 
 
-@rest_controller.route("/upload", methods=['POST'])
+@rest_api_controller.route("/upload", methods=['POST'])
 @pweb_upload_endpoint(response_obj=PersonUploadDTO)
 def upload():
     return ""
 
 
-@rest_controller.route("/list", methods=['GET'])
+@rest_api_controller.route("/list", methods=['GET'])
 @pweb_paginate_endpoint(response_obj=PersonDetailsDTO)
 def list():
     return ""
--- application/pwebe-basic/pwebe_basic/pwebe_basic_registry.py
+++ application/pwebe-basic/pwebe_basic/pwebe_basic_registry.py
@@ -1,7 +1,7 @@
 from pweb import PWebComponentRegister
 from pwebe_basic.controller.basic_controller import basic_controller
 from pwebe_basic.controller.crud_controller import crud_controller
-from pwebe_basic.controller.rest_controller import rest_controller
+from pwebe_basic.controller.rest_api_controller import rest_api_controller
 from pwebe_basic.controller.saas_controller import saas_controller
 
 
@@ -17,4 +17,4 @@ class PWebBasicRegistry(PWebComponentRegister):
         pweb_app.register_blueprint(basic_controller)
         pweb_app.register_blueprint(crud_controller)
         pweb_app.register_blueprint(saas_controller)
-        pweb_app.register_blueprint(rest_controller)
+        pweb_app.register_blueprint(rest_api_controller)
