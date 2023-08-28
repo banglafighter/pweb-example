63f9ccd9bb444828b812574f452df28fe652fb29
hmtmcse <hmtm.cse@gmail.com>
2023-08-28 22:52:02 +0600

    Working on Swagger UI Generation

--- application/pwebe-basic/pwebe_basic/controller/rest_controller.py
+++ application/pwebe-basic/pwebe_basic/controller/rest_controller.py
@@ -1,4 +1,6 @@
-from pweb import Blueprint
+from pweb import Blueprint, pweb_endpoint, pweb_paginate_endpoint
+from pweb_form_rest import pweb_upload_endpoint
+from pwebe_basic.dto.person_dto import PersonCreateDTO, PersonUpdateDTO, PersonDetailsDTO, PersonUploadDTO
 
 url_prefix = "/"
 rest_controller = Blueprint(
@@ -8,6 +10,37 @@ rest_controller = Blueprint(
 )
 
 
-@rest_controller.route("/", methods=['GET'])
-def index():
-    return "Index Page"
+@rest_controller.route("/create", methods=['POST'])
+@pweb_endpoint(request_obj=PersonCreateDTO, pweb_message_response=True)
+def create():
+    return ""
+
+
+@rest_controller.route("/details/<int:id>", methods=['GET'])
+@pweb_endpoint(response_obj=PersonDetailsDTO)
+def details(id: int):
+    return ""
+
+
+@rest_controller.route("/update", methods=['POST'])
+@pweb_endpoint(request_obj=PersonUpdateDTO, pweb_message_response=True)
+def update():
+    return ""
+
+
+@rest_controller.route("/delete/<int:id>", methods=['DELETE'])
+@pweb_endpoint()
+def delete(id: int):
+    return ""
+
+
+@rest_controller.route("/upload", methods=['POST'])
+@pweb_upload_endpoint(response_obj=PersonUploadDTO)
+def upload():
+    return ""
+
+
+@rest_controller.route("/list", methods=['GET'])
+@pweb_paginate_endpoint(response_obj=PersonDetailsDTO)
+def list():
+    return ""
--- application/pwebe-basic/pwebe_basic/dto/person_dto.py
+++ application/pwebe-basic/pwebe_basic/dto/person_dto.py
@@ -1,9 +1,32 @@
 from pweb import fields, APIBase, FileField
+from pwebe_basic.model.Person import Person
 
 
-class PersonDTO(APIBase):
+class PersonDetailsDTO(APIBase):
     first_name = fields.String(required=True, error_messages={"required": "Please enter first name"})
-    last_name = fields.String(allow_none=None)
+    last_name = fields.String(allow_none=True)
     email = fields.Email(required=True, error_messages={"required": "Please enter email."})
-    income = fields.Float(allow_none=None)
-    image = FileField(allow_none=None)
+    income = fields.Float(allow_none=True)
+
+
+class PersonCreateDTO(PersonDetailsDTO):
+    class Meta:
+        model = Person
+        load_instance = True
+
+
+class PersonUpdateDTO(PersonDetailsDTO):
+    class Meta:
+        model = Person
+        load_instance = True
+
+    id = fields.Integer(required=True, error_messages={"required": "Please enter id"})
+
+
+class PersonUploadDTO(APIBase):
+    image = FileField(allow_none=True)
+    id = fields.Integer(required=True, error_messages={"required": "Please enter id"})
+
+    class Meta:
+        model = Person
+        load_instance = True
