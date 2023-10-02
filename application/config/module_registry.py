8019a48d7f4a03d6241f1b2c150a06fa21df492c
hmtmcse <hmtm.cse@gmail.com>
2023-10-02 15:52:40 +0600

    Added Auth structure

--- application/config/module_registry.py
+++ application/config/module_registry.py
@@ -1,4 +1,5 @@
 from pweb import PWebModuleRegister
+from pweb_auth import PWebAuthModule
 from pweb_ssr.pweb_ssr_module import PWebSSRModule
 from pwebe_basic.pwebe_basic_module import PWebBasicModule
 
@@ -8,5 +9,6 @@ class Register(PWebModuleRegister):
     def get_module_list(self) -> list:
         return [
             PWebBasicModule,
-            PWebSSRModule
+            PWebSSRModule,
+            PWebAuthModule
         ]
