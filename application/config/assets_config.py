30fb6f5837e7fe662827dd54aa4239e3a519ee8f
hmtmcse <hmtm.cse@gmail.com>
2023-10-02 10:47:58 +0600

    working on ssr

--- application/config/assets_config.py
+++ application/config/assets_config.py
@@ -4,3 +4,4 @@ from ppy_file_text import FileUtil
 
 class AssetsConfig:
     fileHolder = FileUtil.join_path(Config.UPLOADED_STATIC_RESOURCES, "file-holder")
+    formFile = FileUtil.join_path(Config.UPLOADED_STATIC_RESOURCES, "form-file")
--- application/pwebe-basic/pwebe_basic/form/member_form.py
+++ application/pwebe-basic/pwebe_basic/form/member_form.py
@@ -18,7 +18,7 @@ class MemberDetailsForm(PWebForm):
     email = fields.Email(required=True, error_messages={"required": "Please enter email."})
     age = fields.Integer(allow_none=True)
     dob = fields.Date(allow_none=True, label="Date of Birth")
-    image = FileField(allow_none=True).set_allowed_extension(["jpg", "png"])
+    image = FileField(allow_none=True).set_allowed_extension(["jpg", "png"]).allow_string_name()
     income = fields.Float(required=True, error_messages={"required": "Please enter income"})
     sex = EnumField(Sex, required=True, error_messages={"required": "Please select sex"}, placeholder="Select Sex")
     bio = fields.String(type="textarea", placeholder="Enter Biography", allow_none=True)
