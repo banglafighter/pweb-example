183a384a35211d7b89aef0b67f8c0f1d13b9a2e4
hmtmcse <hmtm.cse@gmail.com>
2023-10-08 15:49:45 +0600

    fixed various issues

--- application/config/app_config.py
+++ application/config/app_config.py
@@ -1,6 +1,25 @@
 from pweb import PWebAppConfig
+from pweb_auth import AuthBase
+from pweb_basic.common.app_ssr_ui_helper import AppSSRUIHelper
+from pweb_form_rest import PWebSSRUIHelper
+from pweb_ssr import SSRTemplateAssets
 
 
 class Config(PWebAppConfig):
     APP_NAME = "PWeb Example"
     PORT: int = 1212
+
+    SWAGGER_TITLE: str = "Swagger Example App"
+    SSR_UI_HELPER: PWebSSRUIHelper = AppSSRUIHelper()
+
+    DEFAULT_TEMPLATE_ASSETS: SSRTemplateAssets = SSRTemplateAssets(
+        name="example",
+        import_name=__name__,
+        template_folder="../template-assets/templates",
+        static_folder="../template-assets/assets",
+        static_url_path="app-assets"
+    )
+
+    TOTAL_ITEM_PER_PAGE: int = 5
+    TABLE_ITEM_PER_PAGE_OPTIONS: str = [5, 10, 25, 50, 100, 200, 500]
+    SYSTEM_AUTH_BASE: AuthBase = AuthBase.EMAIL
--- application/config/module_registry.py
+++ application/config/module_registry.py
@@ -2,6 +2,7 @@ from pweb import PWebModuleRegister
 from pweb_auth import PWebAuthModule
 from pweb_basic.pweb_basic_module import PWebBasicModule
 from pweb_ssr.pweb_ssr_module import PWebSSRModule
+from pweb_ui.pweb_ui_module import PWebUIModule
 
 
 class Register(PWebModuleRegister):
@@ -11,4 +12,5 @@ class Register(PWebModuleRegister):
             PWebSSRModule,
             PWebAuthModule,
             PWebBasicModule,
+            PWebUIModule,
         ]
