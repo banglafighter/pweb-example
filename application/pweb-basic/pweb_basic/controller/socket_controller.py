2ba08500f358c0434b9df4877a1503c754aee547
Touhid Mia <touhid@banglafighter.com>
2024-03-24 12:31:09 +0600

    Working on WebSocket Implementation & Testing

--- application/pweb-basic/pweb_basic/controller/socket_controller.py
+++ application/pweb-basic/pweb_basic/controller/socket_controller.py
@@ -17,5 +17,33 @@ def index():
 
 @socket_controller.route("/notify", methods=['GET'])
 def notify():
-    PWebSocket.notify("eventName", {"json": "Data"})
+    PWebSocket.notify("eventName", "String Notification")
     return "Notified"
+
+
+@socket_controller.route("/notify-dict", methods=['GET'])
+def notify_dict():
+    PWebSocket.notify("eventName", {"dict": "Dictionary Notification"})
+    return "Notified Dict"
+
+
+@socket_controller.route("/notify-message", methods=['GET'])
+def notify_message():
+    PWebSocket.notify_message("Message Notification")
+    return "Notified Message"
+
+
+def plain_text(message):
+    print(f"Called PlainText: {message}")
+
+
+def json_data(json_data):
+    print(type(json_data))
+    print(f"Called JSON Data: {json_data}")
+
+
+@socket_controller.route("/register-event", methods=['GET'])
+def register_event():
+    PWebSocket.register_event("plainText", plain_text)
+    PWebSocket.register_event("jsonData", json_data)
+    return "Register Event"
